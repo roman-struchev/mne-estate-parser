2745d708a67afe13258b3ba6c966ea0c67f6cde9
Roman Struchev <underpender@yandex.ru>
2025-11-18 20:43:58 +0100

    add error handling to SubscriptionScheduler

--- src/main/java/com/estate/parser/service/subscriptions/SubscriptionScheduler.java
+++ src/main/java/com/estate/parser/service/subscriptions/SubscriptionScheduler.java
@@ -33,49 +33,53 @@ public class SubscriptionScheduler {
     public void sendSubscriptions() {
         log.info("Start scheduler to send subscriptions");
 
-        var dateFrom = LocalDate.now().minusDays(1);
-        var updatedAds = adRepository.findAllByLastModifiedGreaterThanEqual(dateFrom.atStartOfDay(),
-                Sort.by("type", "location", "size", "price"));
+        try {
+            var dateFrom = LocalDate.now().minusDays(1);
+            var updatedAds = adRepository.findAllByLastModifiedGreaterThanEqual(dateFrom.atStartOfDay(),
+                    Sort.by("type", "location", "size", "price"));
 
-        subscriptionsConfiguration.getSubscriptions().forEach(s -> {
-            var adsToSend = updatedAds.stream()
-                    .filter(a -> CollectionUtils.isEmpty(s.getDistricts()) || a.getCity() == null || s.getDistricts().contains(a.getCity()))
-                    .filter(a -> CollectionUtils.isEmpty(s.getLocations()) || a.getLocation() == null
-                            || s.getLocations().stream().anyMatch(loc -> distance.apply(loc, a.getLocation()) < 0.1d))
-                    .filter(a -> CollectionUtils.isEmpty(s.getTypes()) || a.getType() == null || s.getTypes().contains(a.getType().getDesc()))
-                    .filter(a -> s.getPriceLessThen() == null || compareUnits(s.getPriceLessThen(), a.getPrice()) >= 0)
-                    .filter(a -> s.getPriceLessThen() == null || compareUnits(s.getPriceMoreThen(), a.getPrice()) <= 0)
-                    .filter(a -> s.getBedroomsLessThen() == null || compareUnits(s.getBedroomsLessThen(), a.getBedrooms()) >= 0)
-                    .filter(a -> s.getBedroomsMoreThen() == null || compareUnits(s.getBedroomsMoreThen(), a.getBedrooms()) <= 0)
-                    .filter(a -> s.getSizeLessThen() == null || compareUnits(s.getSizeLessThen(), a.getSize()) >= 0)
-                    .filter(a -> s.getSizeMoreThen() == null || compareUnits(s.getSizeMoreThen(), a.getSize()) <= 0)
-                    .toList();
-            if (CollectionUtils.isEmpty(adsToSend)) {
-                return;
-            }
+            subscriptionsConfiguration.getSubscriptions().forEach(s -> {
+                var adsToSend = updatedAds.stream()
+                        .filter(a -> CollectionUtils.isEmpty(s.getDistricts()) || a.getCity() == null || s.getDistricts().contains(a.getCity()))
+                        .filter(a -> CollectionUtils.isEmpty(s.getLocations()) || a.getLocation() == null
+                                || s.getLocations().stream().anyMatch(loc -> distance.apply(loc, a.getLocation()) < 0.1d))
+                        .filter(a -> CollectionUtils.isEmpty(s.getTypes()) || a.getType() == null || s.getTypes().contains(a.getType().getDesc()))
+                        .filter(a -> s.getPriceLessThen() == null || compareUnits(s.getPriceLessThen(), a.getPrice()) >= 0)
+                        .filter(a -> s.getPriceLessThen() == null || compareUnits(s.getPriceMoreThen(), a.getPrice()) <= 0)
+                        .filter(a -> s.getBedroomsLessThen() == null || compareUnits(s.getBedroomsLessThen(), a.getBedrooms()) >= 0)
+                        .filter(a -> s.getBedroomsMoreThen() == null || compareUnits(s.getBedroomsMoreThen(), a.getBedrooms()) <= 0)
+                        .filter(a -> s.getSizeLessThen() == null || compareUnits(s.getSizeLessThen(), a.getSize()) >= 0)
+                        .filter(a -> s.getSizeMoreThen() == null || compareUnits(s.getSizeMoreThen(), a.getSize()) <= 0)
+                        .toList();
+                if (CollectionUtils.isEmpty(adsToSend)) {
+                    return;
+                }
 
-            var adsToSendByGroups = adsToSend.stream().collect(Collectors.groupingBy(AdEntity::getType, Collectors.toList()));
-            var messageBody = adsToSendByGroups.entrySet().stream()
-                    .filter(e -> !e.getValue().isEmpty())
-                    .map(e -> {
-                        var index = new AtomicInteger(1);
-                        var groupTitle = e.getKey().getDesc();
-                        var groupBody = e.getValue().stream()
-                                .map(a -> {
-                                    var location = StringUtils.defaultIfBlank(a.getLocation(), "?");
-                                    var size = StringUtils.defaultIfBlank(a.getSize(), "?");
-                                    var price = StringUtils.defaultIfBlank(a.getPrice(), "?");
-                                    return String.format("%s. %s, %s, %s, %se, [%s](%s)", index.getAndIncrement(),
-                                            a.getCity(), location, size, price, a.getSourceCode(), a.getSourceLink());
-                                })
-                                .collect(Collectors.joining("\n"));
-                        return "*%s*\n%s".formatted(groupTitle, groupBody);
-                    }).collect(Collectors.joining("\n\n"));
-            var header = String.format("New ads since %s", dateFrom.format(DateTimeFormatter.ISO_DATE));
-            subscriptionSender.sendToTelegram(s.getTelegramChatIds(), header, messageBody);
-            subscriptionSender.sendToEmail(s.getEmails(), header, messageBody);
-            log.info("Sent notification {}", messageBody);
-        });
+                var adsToSendByGroups = adsToSend.stream().collect(Collectors.groupingBy(AdEntity::getType, Collectors.toList()));
+                var messageBody = adsToSendByGroups.entrySet().stream()
+                        .filter(e -> !e.getValue().isEmpty())
+                        .map(e -> {
+                            var index = new AtomicInteger(1);
+                            var groupTitle = e.getKey().getDesc();
+                            var groupBody = e.getValue().stream()
+                                    .map(a -> {
+                                        var location = StringUtils.defaultIfBlank(a.getLocation(), "?");
+                                        var size = StringUtils.defaultIfBlank(a.getSize(), "?");
+                                        var price = StringUtils.defaultIfBlank(a.getPrice(), "?");
+                                        return String.format("%s. %s, %s, %s, %se, [%s](%s)", index.getAndIncrement(),
+                                                a.getCity(), location, size, price, a.getSourceCode(), a.getSourceLink());
+                                    })
+                                    .collect(Collectors.joining("\n"));
+                            return "*%s*\n%s".formatted(groupTitle, groupBody);
+                        }).collect(Collectors.joining("\n\n"));
+                var header = String.format("New ads since %s", dateFrom.format(DateTimeFormatter.ISO_DATE));
+                subscriptionSender.sendToTelegram(s.getTelegramChatIds(), header, messageBody);
+                subscriptionSender.sendToEmail(s.getEmails(), header, messageBody);
+                log.info("Sent notification {}", messageBody);
+            });
+        } catch (Exception ex) {
+            log.error("Can't send notifications", ex);
+        }
     }
 
     /**
